b372823f0094dab32964e4b25ab7b548a6fc1ae5
zxteloiv <zxteloiv@outlook.com>
2019-11-05 17:37:56 +0800

    add a factory initializer for hyper-parameters.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="trialbot",
-    version="0.0.7",
+    version="0.0.8",
     author="zxteloiv",
     author_email="zxteloiv@gmail.com",
     description="A lightweight training framework for PyTorch",
--- trialbot/training/hparamset.py
+++ trialbot/training/hparamset.py
@@ -6,3 +6,31 @@ class HyperParamSet:
         json_obj = dict((attr, getattr(self, attr)) for attr in dir(self)
                         if hasattr(self, attr) and not attr.startswith('_'))
         return json.dumps(json_obj)
+
+    @staticmethod
+    def common_settings():
+        import os.path
+        hparams = HyperParamSet()
+
+        hparams.DEVICE = -1
+        ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+        hparams.ROOT = ROOT
+        hparams.SNAPSHOT_PATH = os.path.join(ROOT, 'snapshots')
+
+        hparams.LOG_REPORT_INTERVAL = (1, 'iteration')
+        hparams.TRAINING_LIMIT = 500  # in num of epochs
+        hparams.SAVE_INTERVAL = (100, 'iteration')
+        hparams.batch_sz = 128
+        hparams.vocab_min_freq = 3
+
+        hparams.ADAM_LR = 1e-3
+        hparams.ADAM_BETAS = (.9, .98)
+        hparams.ADAM_EPS = 1e-9
+
+        hparams.GRAD_CLIPPING = 5
+
+        hparams.SGD_LR = 1e-2
+        hparams.DATA_PATH = os.path.join(ROOT, 'data')
+
+        return hparams
+
